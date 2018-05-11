e85ab304f1cf2f5bcc6744454562d4af6e910942
woohyunjin06 <woohyunjin06@naver.com>
2018-05-11 20:12:49 +0900

    Separate Toolbar

--- app/src/main/java/com/andteam/andmanager/BackupActivity.kt
+++ app/src/main/java/com/andteam/andmanager/BackupActivity.kt
@@ -1,20 +1,15 @@
 package com.andteam.andmanager
 
-
 import android.support.v7.app.AppCompatActivity
-
 import android.os.Bundle
-
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.appbar_main.*
 
-class  MainActivity : AppCompatActivity() {
+class BackupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_backup)
 
         setSupportActionBar(toolbar)
-
     }
 }
