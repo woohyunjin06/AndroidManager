3a92aaa28b36fe38acbf525f7f6508afda9cceed
woohyunjin06 <woohyunjin06@naver.com>
2018-05-15 21:00:38 +0900

    Add missing resources

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="eb891e50-f540-4e57-9931-dcb5e61aea91")
+public class DataBindingInfo {}
